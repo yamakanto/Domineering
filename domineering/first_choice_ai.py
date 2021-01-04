590df043e1c52180c88237112c4a864b4dc102b2
yamakanto <t.forner@tum.de>
2021-01-04 21:28:32 +0100

    add exception for call without possible moves

--- domineering/first_choice_ai.py
+++ domineering/first_choice_ai.py
@@ -8,4 +8,6 @@ class FirstChoiceAI(DomineeringPlayer):
 
     def get_turn(self, board):
         possible_moves = self.compute_possible_moves(board, self.vertical)
+        if not possible_moves:
+            raise ValueError('No possible move left!')
         return possible_moves[0]
