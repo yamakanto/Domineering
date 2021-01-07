b0e670b2b9e0674cf99e6b48b57d840791218a3a
yamakanto <t.forner@tum.de>
2021-01-07 13:38:25 +0100

    removed unnecessary import

--- domineering/__main__.py
+++ domineering/__main__.py
@@ -5,8 +5,10 @@ from domineering.strategy_ai import StrategyAI
 from domineering.possible_moves_evaluation import PossibleMovesEvaluation
 from domineering.real_possible_moves_evaluation import RealPossibleMovesEvaluation
 from domineering.safe_real_possible_moves_evaluation import SafeRealPossibleMovesEvaluation
+from domineering.lookahead_depth import ProgressiveLookaheadDepth
 import cProfile
-from pstats import Stats, SortKey
+from pstats import Stats
+from time import perf_counter
 
 
 def start_game():
@@ -17,10 +19,12 @@ def start_game():
     eval1 = PossibleMovesEvaluation()
     eval2 = RealPossibleMovesEvaluation()
     eval2 = SafeRealPossibleMovesEvaluation()
-    player1 = StrategyAI(True, eval2, 2)
+    lookahead_func = ProgressiveLookaheadDepth([1, 2, 3], [0, 15, 30])
+    player1 = StrategyAI(True, eval2, lookahead_func)
 
-    player2 = StrategyAI(False, eval2, 2)
-    game = DomineeringGame(7, player1, player2)
+    player2 = StrategyAI(False, eval2, lookahead_func)
+    game = DomineeringGame(5, player1, player2)
+    # game._play_turn()
     game.play()
 
 
@@ -33,7 +37,11 @@ if __name__ == '__main__':
         with open('profiling_stats.txt', 'w') as stream:
             stats = Stats(pr, stream=stream)
             stats.strip_dirs()
-            stats.sort_stats('time')
+            stats.sort_stats()
+            stats.dump_stats('.prof_stats')
             stats.print_stats()
     else:
+        start_time = perf_counter()
         start_game()
+        end_time = perf_counter()
+        print(f'Finished after {end_time-start_time}.')
