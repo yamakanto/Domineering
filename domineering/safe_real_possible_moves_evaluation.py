8cfa15a88cfd72aa675ee864b52bbd7c681c9ac8
yamakanto <t.forner@tum.de>
2021-01-07 13:45:59 +0100

    add option to use precomputed safe, possible moves

--- domineering/safe_real_possible_moves_evaluation.py
+++ domineering/safe_real_possible_moves_evaluation.py
@@ -5,9 +5,18 @@ from domineering.board_analyzer import count_safe_moves_horizontal, count_safe_m
 
 class SafeRealPossibleMovesEvaluation(RealPossibleMovesEvaluation):
 
-    def __call__(self, board):
-        possible_moves_diff = self._compute_possible_moves_diff(board)
-        safe_moves_diff = self._compute_safe_move_diff(board)
+    def __call__(self, board, possible_moves_dict=None, safe_moves_dict=None):
+        if possible_moves_dict:
+            possible_moves_diff = len(
+                possible_moves_dict[True]) - len([possible_moves_dict[False]])
+        else:
+            possible_moves_diff = self._compute_possible_moves_diff(board)
+        if safe_moves_dict:
+            safe_moves_diff = len(
+                safe_moves_dict[True]) - len([safe_moves_dict[False]])
+        else:
+            safe_moves_diff = self._compute_safe_move_diff(board)
+
         return 2 * safe_moves_diff + possible_moves_diff
 
     def _compute_safe_move_diff(self, board):
